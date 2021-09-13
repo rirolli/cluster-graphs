a4a541389acfa7eb137cacc9c0fec0f23911603a
Riccardo Ungaro <riccardo.ung@outlook.it>
2021-09-13 17:47:46 +0200

    Migliorato il sistema di pulizia dei grafi

--- pulisci_data.py
+++ pulisci_data.py
@@ -1,10 +1,23 @@
-from json import load
+from json import load, dump
 
 with open('data.json', 'r') as f:
     data = load(f)
 
-link_list = [value for elem in data[0]['links'] for value in elem.values()]
+print(len(data))
 
-for d in data[0]['nodes']:
-    if d['name'] not in link_list:
-        print(d['name'])
+for i in range(len(data)):
+
+    link_list = [value for elem in data[i]['links'] for value in elem.values()]
+    nodes_not_linked = []
+
+    for d in data[i]['nodes']:
+        if d['name'] not in link_list:
+            nodes_not_linked.append(d['name'])
+
+    print(nodes_not_linked)
+
+
+    data[i] = [d for d in data[i]['nodes'] if d['name'] not in nodes_not_linked]
+
+    with open('data2.json', 'w') as f:
+        dump(data, f)
